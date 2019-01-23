2a0e2502f435df071c0edf5a17eba2ba9a5191f0
ahuglajbclajep <ahuglajbclajep@gmail.com>
2019-01-23 11:39:18 +0900

    fix build option

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,24 +7,24 @@ interface Recv<V, S> {
 interface Close {
     kind: "close";
 }
-declare type C2W<V, Cont extends {
+interface C2W<V, Cont extends {
     client: any;
     worker: any;
-}> = {
+}> {
     client: Send<V, Cont["client"]>;
     worker: Recv<V, Cont["worker"]>;
-};
-declare type W2C<V, Cont extends {
+}
+interface W2C<V, Cont extends {
     client: any;
     worker: any;
-}> = {
+}> {
     client: Recv<V, Cont["client"]>;
     worker: Send<V, Cont["worker"]>;
-};
-declare type Fin = {
+}
+interface Fin {
     client: Close;
     worker: Close;
-};
+}
 declare function send<V, S>(port: Send<V, S>, value: V): S;
 declare function recv<V, S>(port: Recv<V, S>): Promise<[V, S]>;
 declare function close(port: Close): void;
--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,18 @@ interface Close {
   kind: "close";
 }
 
-type C2W<V, Cont extends { client: any; worker: any }> = {
+interface C2W<V, Cont extends { client: any; worker: any }> {
   client: Send<V, Cont["client"]>;
   worker: Recv<V, Cont["worker"]>;
-};
-type W2C<V, Cont extends { client: any; worker: any }> = {
+}
+interface W2C<V, Cont extends { client: any; worker: any }> {
   client: Recv<V, Cont["client"]>;
   worker: Send<V, Cont["worker"]>;
-};
-type Fin = { client: Close; worker: Close };
+}
+interface Fin {
+  client: Close;
+  worker: Close;
+}
 
 function send<V, S>(port: Send<V, S>, value: V): S {
   ((port as any) as MessagePort).postMessage(value);
