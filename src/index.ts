ad7d316c16c9f9b0d1daa3eec0d2ed939e8f7103
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-11-17 15:52:50 +0900

    add mpst.ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,29 @@
-interface Send<V, S> {
-  kind: "send";
-}
-interface Recv<V, S> {
-  kind: "recv";
-}
-interface Close {
-  kind: "close";
-}
+import type {
+  Close,
+  Finish,
+  Offer,
+  OfferLocals,
+  Select,
+  SelectLocals,
+  To,
+} from "./mpst";
 
-interface C2W<V, Cont extends { client: any; worker: any }> {
-  client: Send<V, Cont["client"]>;
-  worker: Recv<V, Cont["worker"]>;
-}
-interface W2C<V, Cont extends { client: any; worker: any }> {
-  client: Recv<V, Cont["client"]>;
-  worker: Send<V, Cont["worker"]>;
-}
-interface Fin {
-  client: Close;
-  worker: Close;
-}
+declare function send<
+  R extends string,
+  LS extends SelectLocals,
+  L extends keyof LS
+>(
+  channel: Select<R, LS>,
+  role: R,
+  label: L,
+  value: Parameters<LS[L]>[0]
+): ReturnType<LS[L]>;
 
-function send<V, S>(port: Send<V, S>, value: V): S {
-  ((port as any) as MessagePort).postMessage(value);
-  return port as any;
-}
+declare function recv<R extends string, LS extends OfferLocals>(
+  channel: Offer<R, LS>,
+  role: R
+): Promise<LS>;
 
-function recv<V, S>(port: Recv<V, S>): Promise<[V, S]> {
-  return new Promise(
-    resolve =>
-      (((port as any) as MessagePort).onmessage = e =>
-        resolve([e.data, port as any]))
-  );
-}
+declare function close(channel: Close): void;
 
-function close(port: Close): void {}
-
-export { C2W, W2C, Fin, send, recv, close };
+export { send, recv, close, To, Finish };
