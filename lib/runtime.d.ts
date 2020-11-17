38cbae9c452e6ca412dfd8174deac88ff2351866
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-11-17 18:32:15 +0900

    fix for npm run build

---
+++ lib/runtime.d.ts
@@ -0,0 +1,14 @@
+import type { Select, SelectLocals, Offer, OfferLocals, Close } from "./mpst";
+declare type Message = [string, unknown];
+declare type Ports = Record<string, MessagePort>;
+declare type Context = {
+    ports: Ports;
+    queues: Record<string, Message[]>;
+};
+declare type Workers = Record<string, Window | Worker>;
+declare type SelfOrWorkers = DedicatedWorkerGlobalScope | Workers;
+declare function init(selfOrWorkers: SelfOrWorkers): Promise<Context | undefined>;
+declare function send<R extends string, LS extends SelectLocals, L extends keyof LS>(channel: Select<R, LS>, role: R, label: L, value: Parameters<LS[L]>[0]): ReturnType<LS[L]>;
+declare function recv<R extends string, LS extends OfferLocals>(channel: Offer<R, LS>, role: R): Promise<LS>;
+declare function close(channel: Close): void;
+export { init, send, recv, close };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-import type { To, Finish } from "./mpst";
-import { init, send, recv, close } from "./runtime";
-
-export { init, send, recv, close, To, Finish };
+export type { To, Finish } from "./mpst";
+export { init, send, recv, close } from "./runtime";
