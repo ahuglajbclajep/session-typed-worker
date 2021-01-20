6bfa0afd749951b49d3bca91f6c34e064807cbb4
ahuglajbclajep <ahuglajbclajep@gmail.com>
2021-01-21 08:49:20 +0900

    impl todo list

--- examples/todo-app/src/index.tsx
+++ examples/todo-app/src/index.tsx
@@ -1,3 +1,54 @@
-import { render } from "preact";
+import { FunctionComponent, render } from "preact";
+import { useCallback, useRef, useState } from "preact/hooks";
+import type { Contents } from "./protocols";
 
-render(<h1>Hello, Preact!</h1>, document.getElementById("root")!);
+const AddTodo: FunctionComponent<{ add: (todo: string) => void }> = (props) => {
+  const ref = useRef<HTMLInputElement>();
+
+  const handleClick = useCallback(() => {
+    props.add(ref.current.value);
+  }, [props.add, ref.current]);
+
+  return (
+    <>
+      <input type="text" ref={ref} />
+      <button onClick={handleClick}>add</button>
+    </>
+  );
+};
+
+const Todo: FunctionComponent<{
+  todo: string;
+  rm: () => void;
+}> = (props) => {
+  return <li onClick={props.rm}>{props.todo}</li>;
+};
+
+const App: FunctionComponent = () => {
+  const [contents, setContents] = useState<Contents>([]);
+
+  const addTodo = useCallback((todo: string) => {
+    const createdAt = Date.now();
+    setContents((prev) => [{ createdAt, todo }, ...prev]);
+  }, []);
+
+  const rmTodo = useCallback(
+    (createdAt: number) => () => {
+      setContents((prev) => prev.filter((c) => c.createdAt !== createdAt));
+    },
+    []
+  );
+
+  const items = contents.map((c) => (
+    <Todo key={c.createdAt} todo={c.todo} rm={rmTodo(c.createdAt)} />
+  ));
+  return (
+    <>
+      <h1>todo</h1>
+      <AddTodo add={addTodo} />
+      <ul>{items}</ul>
+    </>
+  );
+};
+
+render(<App />, document.getElementById("root")!);
